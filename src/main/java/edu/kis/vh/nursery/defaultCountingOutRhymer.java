49510450b4756d94319271ffb9849c6cee0210f8
anhelin2a <mendohraloanhelina@gmail.com>
2024-05-09 15:03:23 +0200

    Fixed issue

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int MAX_CAPACITY = 12; 
+    public static final int EMPTY = -1;
+    public static final int MAX_INDEX = DEFAULT_CAPACITY - 1;
 
-    private int total = -1;
+    private int[] numbers = new int[MAX_CAPACITY];
+
+    private int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,11 +16,11 @@ public class defaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_INDEX;
     }
 
     protected int peekaboo() {
