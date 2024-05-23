00ac16c0ede3b84dd9adbe6292c4b8abde885ac9
anhelin2a <mendohraloanhelina@gmail.com>
2024-05-23 11:56:31 +0200

    Deleted unused setters

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -16,24 +16,10 @@ public class Node {
     public int getValue() {
         return value;
     }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
     public Node getPrev() {
         return prev;
     }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
     public Node getNext() {
         return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
     }
 }
